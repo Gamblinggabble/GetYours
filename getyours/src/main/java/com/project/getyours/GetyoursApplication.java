a01e5928f806dd438b54c715de4627f6a664d3d1
Gabriela Dimcheva <gabriela.dim_cheva@abv.bg>
2023-04-21 20:09:14 +0300

    Initial commit

---
+++ getyours/src/main/java/com/project/getyours/GetyoursApplication.java
@@ -0,0 +1,13 @@
+package com.project.getyours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetyoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetyoursApplication.class, args);
+	}
+
+}
---
+++ getyours/src/test/java/com/project/getyours/GetyoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.getyours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetyoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
